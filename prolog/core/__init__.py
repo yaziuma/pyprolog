d5376e8359d398966a87a429feefd4031240209b
yuichi.ide <yuichi.ide@funlead.co.jp>
2025-05-23 10:40:26 +0900

    リファクタリング

---
+++ prolog/core/__init__.py
@@ -0,0 +1,12 @@
+# prolog/core/__init__.py
+from .types import Variable, Term, Rule, Conjunction
+from .binding_environment import BindingEnvironment
+from .errors import InterpreterError, ScannerError, ParserError
+from .merge_bindings import merge_bindings
+
+__all__ = [
+    'Variable', 'Term', 'Rule', 'Conjunction',
+    'BindingEnvironment',
+    'InterpreterError', 'ScannerError', 'ParserError',
+    'merge_bindings'
+]
